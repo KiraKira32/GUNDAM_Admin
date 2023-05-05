fc5db3371c217c14182cce02e6c969cf89010b00
KiraKira32 <chiennnk32@gmail.com>
2023-05-05 14:54:44 +0800

    deploy

--- assets/NotFound-79a50424.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o,c,a as e}from"./index-51dddea8.js";const s={},n={class:"container"},a=e("div",{class:"text-center my-1"},[e("h1",null,"404 Not Found"),e("hr"),e("p",null,"不存在的頁面錯誤！")],-1),r=[a];function _(d,l){return o(),c("div",n,r)}const u=t(s,[["render",_]]);export{u as default};
